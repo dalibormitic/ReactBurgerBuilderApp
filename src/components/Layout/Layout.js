ea56b2ac0a05ca6ccb8edc7d2da30c65cbf6e882
Dalibor <dalibor.mitic997@gmail.com>
2020-02-18 23:16:35 +0100

    Added DrawerToggle component and some styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.module.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+          return {showSideDrawer: !prevState.showSideDrawer};      
+        });
+    }
+
+    render(){
+        return(
+            <>
+            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+            </>
+        );
+    }    
+};
+
+export default Layout;
