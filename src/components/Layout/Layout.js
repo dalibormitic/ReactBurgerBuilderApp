66c1458a1fc83b5d7e7e1622edfa723ca219849c
Dalibor Mitić <35170405+dalibormitic@users.noreply.github.com>
2020-03-18 20:00:53 +0100

    Logout functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,34 +1,46 @@
-import React, {Component} from 'react';
-import classes from './Layout.module.css'
+import React, { Component } from 'react';
+import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import { connect } from 'react-redux';
 
 class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
+  state = {
+    showSideDrawer: false
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false})
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-          return {showSideDrawer: !prevState.showSideDrawer};      
-        });
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
 
-    render(){
-        return(
-            <>
-            <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
-            <SideDrawer closed={this.sideDrawerClosedHandler} open={this.state.showSideDrawer}/>
-            <main className={classes.Content}>
-                {this.props.children}
-            </main>
-            </>
-        );
-    }    
+  render() {
+    return (
+      <>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <SideDrawer
+          isAuth={this.props.isAuthenticated}
+          closed={this.sideDrawerClosedHandler}
+          open={this.state.showSideDrawer}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
 };
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,25 +4,25 @@ import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = (props) => {
-    let attachedClasses = [classes.SideDrawer, classes.Close];    
-    if(props.open){
-        attachedClasses = [classes.SideDrawer, classes.Open];
-    }
+const sideDrawer = props => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
 
-    return(
-        <>
-            <Backdrop show={props.open} clicked={props.closed} />
-            <div className={attachedClasses.join(' ')}>
-                <div className={classes.Logo}>
-                    <Logo />
-                </div>
-                <nav>
-                    <NavigationItems />
-                </nav>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems isAuthenticated={props.isAuth} />
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default sideDrawer;
