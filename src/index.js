be47eb2f12ee5ff702f7f9a440508602c4d087a6
Dalibor <dalibor.mitic997@gmail.com>
2020-03-24 23:19:04 +0100

    Outsourcing some side-effects to Redux Saga

--- src/index.js
+++ src/index.js
@@ -10,21 +10,27 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import thunk from 'redux-thunk';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import createSagaMiddleware from 'redux-saga';
+import { watchAuth } from "./store/sagas/index";
 
 const composeEnhancers =
   process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : null || compose;
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      : null || compose;
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
